709ea0dd3aecd48d0c98c9abd8661631da928876
FiorellaTriverio <fiorella.triverio@mercadolibre.com>
2021-05-26 15:39:20 -0300

    Subiendo ejercicio 4

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -7,51 +7,21 @@ public class Ejercicio4 {
      * siendo n un valor que el usuario ingresará por consola.
      */
 
-
-    /**
-     * Otra solución pero con String y char
-     */
-    /*public static void main(String[] args){
-
-        int n, m, d;
-
-        System.out.println("Ingrese valor n, m y d: ");
-        Scanner entradaEscaner = new Scanner (System.in);
-        n = entradaEscaner.nextInt();
-        m = entradaEscaner.nextInt();
-        d = entradaEscaner.nextInt();
-
-        int numero = 0;
-
-        while(n>0){
-
-            if(esValido(m ,d, numero)){
-                System.out.println(numero);
-                n--;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número");
+        int n = sc.nextInt();
+        for (int i = 2; i <= n; i++) {
+            int contador = 0;
+            for (int j = 1; j <= i; j++) {
+                if(i%j==0) {
+                    contador++;
+                }
             }
-
-            numero++;
+            if(contador <= 2){
+                System.out.println(i);
+            }
         }
-
-        entradaEscaner.close();
-
     }
-
-    public static boolean esValido(int cantDigitos, int digito, int numero){
-
-        String numero_str = String.valueOf(numero);
-        char[] digitos_numero = numero_str.toCharArray();
-
-        int cont = 0;
-
-        for(int i = 0; i < digitos_numero.length; i++) {
-            if(Character.getNumericValue(digitos_numero[i]) == digito) cont++;
-        }
-
-        if(cont==cantDigitos) return true;
-
-        return false;
-    }*/
-
 
 }
--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -42,4 +42,49 @@ public class Ejercicio5 {
         }
     }
 
+    /**
+     * Otra solución pero con String y char
+     */
+    /*public static void main(String[] args){
+
+        int n, m, d;
+
+        System.out.println("Ingrese valor n, m y d: ");
+        Scanner entradaEscaner = new Scanner (System.in);
+        n = entradaEscaner.nextInt();
+        m = entradaEscaner.nextInt();
+        d = entradaEscaner.nextInt();
+
+        int numero = 0;
+
+        while(n>0){
+
+            if(esValido(m ,d, numero)){
+                System.out.println(numero);
+                n--;
+            }
+
+            numero++;
+        }
+
+        entradaEscaner.close();
+
+    }
+
+    public static boolean esValido(int cantDigitos, int digito, int numero){
+
+        String numero_str = String.valueOf(numero);
+        char[] digitos_numero = numero_str.toCharArray();
+
+        int cont = 0;
+
+        for(int i = 0; i < digitos_numero.length; i++) {
+            if(Character.getNumericValue(digitos_numero[i]) == digito) cont++;
+        }
+
+        if(cont==cantDigitos) return true;
+
+        return false;
+    }*/
+
 }
